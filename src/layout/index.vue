7cee55f68597ceea3ee3c3b06012306980e30739
刘昊 <liu.hao8@neolink.com>
2022-09-07 16:15:59 +0800

    Update something

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -11,7 +11,7 @@
                     </div>
                 </div>
             </el-col>
-            <!--<el-col class="header-right" :span="18">
+            <!--<el-col class="header-right" :span="17">
             </el-col>-->
         </el-row>
     </div>
@@ -20,10 +20,10 @@
         <div class="main-top">
             <el-row>
                 <el-col :span="5" :offset="1">
-                    <el-card class="common-cards">
+                    <el-card class="common-cards" style="cursor: pointer;">
                         <div style="margin-top: -15px;">
                             <div style="display: inline-block;">
-                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #2d5fff;">
+                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #409EFF;">
                                     <el-image :src="blockUrl" style="object-fit: cover; height:60%;"></el-image>
                                 </span>
                             </div>
@@ -43,10 +43,10 @@
                         </div>
                     </el-card>
 
-                    <el-card class="common-cards" style="margin-top: 15px;">
+                    <el-card class="common-cards" style="margin-top: 15px; cursor: pointer;">
                         <div style="margin-top: -15px;">
                             <div style="display: inline-block;">
-                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #3dd070;">
+                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #67C23A;">
                                     <el-image :src="organizationUrl" style="object-fit: cover; height:60%;"></el-image>
                                 </span>
                             </div>
@@ -68,10 +68,10 @@
                 </el-col>
 
                 <el-col :span="5">
-                    <el-card class="common-cards">
+                    <el-card class="common-cards" style="cursor: pointer;">
                         <div style="margin-top: -15px;">
                             <div style="display: inline-block;">
-                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #2d5fff;">
+                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #409EFF;">
                                     <el-image :src="transactionUrl" style="object-fit: cover; height:60%;"></el-image>
                                 </span>
                             </div>
@@ -91,10 +91,10 @@
                         </div>
                     </el-card>
 
-                    <el-card class="common-cards" style="margin-top: 15px; cursor: pointer;" @click="showDetailInfo('node')">
+                    <el-card class="common-cards" style="margin-top: 15px; cursor: pointer;" @click="dialogNodeVisible = true">
                         <div style="margin-top: -15px;">
                             <div style="display: inline-block;">
-                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #3dd070;">
+                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #67C23A;">
                                     <el-image :src="nodeUrl" style="object-fit: cover; height:60%;"></el-image>
                                 </span>
                             </div>
@@ -116,10 +116,10 @@
                 </el-col>
 
                 <el-col :span="5">
-                    <el-card class="common-cards">
+                    <el-card class="common-cards" style="cursor: pointer;">
                         <div style="margin-top: -15px;">
                             <div style="display: inline-block;">
-                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #2d5fff;">
+                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #409EFF;">
                                     <el-image :src="contractkUrl" style="object-fit: cover; height:60%;"></el-image>
                                 </span>
                             </div>
@@ -139,10 +139,10 @@
                         </div>
                     </el-card>
 
-                    <el-card class="common-cards" style="margin-top: 15px;">
+                    <el-card class="common-cards" style="margin-top: 15px; cursor: pointer;">
                         <div style="margin-top: -15px;">
                             <div style="display: inline-block;">
-                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #3dd070;">
+                                <span class="el-avatar el-avatar--circle" style="--el-avatar-size: 50px; background: #67C23A;">
                                     <el-image :src="userUrl" style="object-fit: cover; height:60%;"></el-image>
                                 </span>
                             </div>
@@ -163,7 +163,10 @@
                     </el-card>
                 </el-col>
 
-                <el-col :span="8">
+                <el-col :span="7">
+                    <el-card class="common-cards" id="myChartCard" style="cursor: pointer; margin: 0px 20px 0px 15px; padding: 0px;" :body-style="{ padding: '0px'}">
+                        <div id="myChart"></div>
+                    </el-card>
                 </el-col>
             </el-row>
         </div>
@@ -185,10 +188,10 @@
                                 <el-col :span="6">
                                     <el-card style="margin-top: 10px; margin-right: 20px;">
                                         <template #header>
-                                            <span style="font-size: 22px; font-weight: 600;">
+                                            <span style="font-size: 18px; font-weight: 600;">
                                                 # 22
                                             </span>
-                                            <span style="font-size: 14px; color: #999; float: right; line-height: 32px;">
+                                            <span style="font-size: 12px; color: #999; float: right; line-height: 24px;">
                                                 09-06 15:06
                                             </span>
                                         </template>
@@ -200,10 +203,10 @@
                                 <el-col :span="6">
                                     <el-card style="margin-top: 10px; margin-right: 20px;">
                                         <template #header>
-                                            <span style="font-size: 22px; font-weight: 600;">
+                                            <span style="font-size: 18px; font-weight: 600;">
                                                 # 21
                                             </span>
-                                            <span style="font-size: 14px; color: #999; float: right; line-height: 32px;">
+                                            <span style="font-size: 12px; color: #999; float: right; line-height: 24px;">
                                                 09-06 15:06
                                             </span>
                                         </template>
@@ -215,10 +218,10 @@
                                 <el-col :span="6">
                                     <el-card style="margin-top: 10px; margin-right: 20px;">
                                         <template #header>
-                                            <span style="font-size: 22px; font-weight: 600;">
+                                            <span style="font-size: 18px; font-weight: 600;">
                                                 # 20
                                             </span>
-                                            <span style="font-size: 14px; color: #999; float: right; line-height: 32px;">
+                                            <span style="font-size: 12px; color: #999; float: right; line-height: 24px;">
                                                 09-06 15:06
                                             </span>
                                         </template>
@@ -230,10 +233,10 @@
                                 <el-col :span="6">
                                     <el-card style="margin-top: 10px; margin-right: 20px;">
                                         <template #header>
-                                            <span style="font-size: 22px; font-weight: 600;">
+                                            <span style="font-size: 18px; font-weight: 600;">
                                                 # 19
                                             </span>
-                                            <span style="font-size: 14px; color: #999; float: right; line-height: 32px;">
+                                            <span style="font-size: 12px; color: #999; float: right; line-height: 24px;">
                                                 09-06 15:06
                                             </span>
                                         </template>
@@ -249,7 +252,7 @@
             </el-row>
         </div>
 
-        <!--<div class="main-bottom">
+        <div class="main-bottom">
             <el-row style="margin-top: 25px;">
                 <el-col :span="11" :offset="1">
                     <div class="main-bottom-left">
@@ -265,10 +268,10 @@
                         <div class="main-bottom-left-body">
                             <el-card v-for="i in 10" :key="i" class="box-card" style="margin-top: 10px; margin-right: 20px;">
                                 <template #header>
-                                    <el-link type="primary">
+                                    <el-button link type="primary">
                                         se93u7K3BybK78pIZyQGtPtISIJAaTrxscy8Wv4a4fqcm4U3pu
-                                    </el-link>
-                                    <span style="font-size: 14px; color: #999; float: right;">
+                                    </el-button>
+                                    <span style="font-size: 12px; color: #999; float: right;">
                                         09-06 15:06
                                     </span>
                                 </template>
@@ -294,9 +297,9 @@
                         <div class="main-bottom-right-body">
                             <el-card v-for="i in 3" :key="i" class="box-card" style="margin-top: 10px; margin-right: 20px;">
                                 <template #header>
-                                    <el-link type="primary">
+                                    <el-button link type="primary">
                                         se93u7K3BybK78pIZyQGtPtISIJAaTrxscy8Wv4a4fqcm4U3pu
-                                    </el-link>
+                                    </el-button>
                                     <span style="font-size: 14px; color: #999; float: right;">
                                         09-06 15:06
                                     </span>
@@ -309,17 +312,48 @@
                     </div>
                 </el-col>
             </el-row>
-        </div>-->
+        </div>
     </div>
 
     <div class="footer">
 
     </div>
+
+    <el-dialog v-model="dialogNodeVisible" :width="dialogNodeWidth">
+        <template #header="{ titleId, titleClass }">
+            <div :id="titleId" :class="titleClass">
+                节点数：4（健康节点数：<span style="color: #67C23A;">3</span>，损坏节点数：<span style="color: #F56C6C;">1</span>）
+            </div>
+        </template>
+        <el-table :data="gridNodeData" border style="margin-top: -10px; margin-bottom: 0px;">
+            <el-table-column property="nodeName" label="节点名称" />
+            <el-table-column property="nodeAddress" label="节点地址" />
+            <el-table-column property="nodeHealty" label="节点状态" width="100">
+                <template  #default="scope">
+                    <el-tag
+                        :type="scope.row.nodeHealty === '健康' ? 'success' : 'danger'"
+                        disable-transitions
+                        >{{ scope.row.nodeHealty }}</el-tag
+                        >
+                </template>
+            </el-table-column>
+            <el-table-column fixed="right" label="更多操作" width="150">
+                <template #default>
+                    <el-button link type="primary" size="small" @click="handleClick">查看明细</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <!--<template #footer>
+            <el-button type="primary" @click="dialogNodeVisible = false">确定</el-button>
+        </template>-->
+    </el-dialog>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "@vue/reactivity"
 import { InfoFilled } from '@element-plus/icons-vue'
+import * as echarts from 'echarts'
+import { onMounted } from 'vue'
 
 const headerImageFit = ref("contain")
 const headerImageSrc = ref("./src/assets/logo.png")
@@ -330,13 +364,90 @@ const organizationUrl = ref("./src/assets/organization.png")
 const transactionUrl = ref("./src/assets/transaction.png")
 const userUrl = ref("./src/assets/user.png")
 
-const locationToGitHub = function () {
+const dialogNodeVisible = ref(false)
+const dialogNodeWidth = ref("50%")
+const gridNodeAlign = ref("center")
+const gridNodeData = [
+  {
+    nodeName: '北京M6数据中心',
+    nodeAddress: 'http://10.22.50.171:5432',
+    nodeHealty: '健康',
+  },
+  {
+    nodeName: '北京香山数据中心',
+    nodeAddress: 'http://10.22.50.171:5433',
+    nodeHealty: '健康',
+  },
+  {
+    nodeName: '北京B28数据中心',
+    nodeAddress: 'http://10.22.50.171:5434',
+    nodeHealty: '健康',
+  },
+  {
+    nodeName: '北京大兴星光影视城数据中心',
+    nodeAddress: 'http://10.22.50.171:5435',
+    nodeHealty: '损坏',
+  },
+]
+
+const locationToGitHub = () => {
     location.href = "https://github.com/vmeta42/metatoc"
 }
-const showDetailInfo = function (type) {
+const showDetailInfo = type => {
     if (type == "node") {
     }
 }
+const handleClick = () => {
+  console.log('click')
+}
+const drawChart = () => {
+    type EChartsOption = echarts.EChartsOption
+
+    var chartDom = document.getElementById('myChart')!;
+    var myChart = echarts.init(chartDom, null, {
+        width: document.getElementById('myChart').offsetWidth,
+        height: 238,
+    });
+    var option: EChartsOption;
+
+    option = {
+        title: {
+            text: '最新24H交易',
+            top: 12,
+            left: 10,
+            textStyle: {
+                fontSize: 14,
+            },
+        },
+        grid: {
+            top: '24%',
+            left: '5%',
+            right: '5%',
+            bottom: '8%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: 'line',
+            smooth: true
+            }
+        ]
+    };
+
+    option && myChart.setOption(option);
+}
+
+onMounted(() => {
+    drawChart()
+})
 </script>
 
 <style scoped>
@@ -374,14 +485,14 @@ const showDetailInfo = function (type) {
 
 .main-middle-left-header, .main-bottom-left-header, .main-bottom-right-header {
     border-left-width: 6px;
-    border-left-color: #2d5fff;
+    border-left-color: #409EFF;
     border-left-style: solid;
     margin-bottom: 18px;
 }
 
 .main-middle-left-header-title, .main-bottom-left-header-title, .main-bottom-right-header-title {
     font-size: 20px;
-    color:#2d5fff;
+    color:#409EFF;
     font-weight: bold;
     margin-left: 15px;
 }
--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-
 createApp(App)
 .use(router)
 .use(ElementPlus, {zhCn})
